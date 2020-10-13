8add8edda620672d02a7f137a428c6ad25c0aaf7
Jorn Neijssen <jorn.neijssen@hotmail.com>
2020-10-13 11:23:58 +0200

    client toegevoegd
    client toegevoegd

--- CSharp-Eindopdracht/TCPClient/Program.cs
+++ CSharp-Eindopdracht/TCPClient/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
 
 namespace TCPClient
 {
@@ -6,7 +9,41 @@ namespace TCPClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool stopreceived = false;
+            Console.WriteLine("Looking for connection");
+            TcpClient client = new TcpClient("", 5252);
+            Console.WriteLine("Type (stop) to stop connection");
+            while (!stopreceived)
+            {
+                Console.Write("Send something to the server: ");
+                string clientmes = Console.ReadLine();
+
+                WriteTextMessage(client, clientmes);
+
+                string response = ReadTextMessage(client);
+                Console.WriteLine("Response: " + response);
+                stopreceived = response.Equals("stop");
+            }
         }
+
+        public static void WriteTextMessage(TcpClient client, string message)
+        {
+            var stream = new StreamWriter(client.GetStream(), Encoding.ASCII);
+            {
+                stream.WriteLine(message);
+                stream.Flush();
+            }
+        }
+
+        public static string ReadTextMessage(TcpClient client)
+        {
+            var stream = new StreamReader(client.GetStream(), Encoding.ASCII);
+            {
+                return stream.ReadLine();
+            }
+        }
+
+
     }
 }
+
--- CSharp-Eindopdracht/TCPServer/Program.cs
+++ CSharp-Eindopdracht/TCPServer/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+
 
 namespace TCPServer
 {
@@ -6,7 +13,41 @@ namespace TCPServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool TryIp;
+            IPAddress localhost = IPAddress.TryParse("", out TryIp );
+            TcpListener tcplistener = new TcpListener(localhost, 5252);
+
+            tcplistener.Start();
+
+            while(true)
+            {
+
+            }
+
+        }
+
+        public static Encoding encoding = Encoding.UTF8;
+
+        public static string ReadTextMessage(NetworkStream networkStream)
+        {
+            StreamReader stream = new StreamReader(networkStream, encoding);
+            return stream.ReadLine();
+            var stream = new StreamReader(networkStream, encoding);
+            {
+                return stream.ReadLine();
+            }
+        }
+
+        public static void WriteTextMessage(NetworkStream networkStream, string message)
+        {
+            StreamWriter stream = new StreamWriter(networkStream, encoding);
+            stream.WriteLine(message);
+            stream.Flush();
+            using (var stream = new StreamWriter(networkStream, encoding, -1, true))
+            {
+                stream.WriteLine(message);
+                stream.Flush();
+            }
         }
     }
 }
